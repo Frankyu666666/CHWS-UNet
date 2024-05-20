c8a46871866b6f27aaf7067a9f95f5117e73263c
Frankyu666666 <102924580+Frankyu666666@users.noreply.github.com>
2024-05-20 16:49:50 +0800

    Update ECAEModule.py

--- ECAEModule.py
+++ ECAEModule.py
@@ -4,38 +4,6 @@ import torch
 import math
 from model.UNet_LCAM import ChannelAttention
 
-# class ChannelAttention(nn.Module):
-#     def __init__(self, in_planes, ratio=8, gamma=2, b=1):
-#         super(ChannelAttention, self).__init__()
-#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-#         self.max_pool = nn.AdaptiveMaxPool2d(1)
-#         self.in_planes = in_planes
-#         # self.fc = nn.Sequential(nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False),
-#         #                         nn.ReLU(),
-#         #                         nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False))
-#         # self.sigmoid = nn.Sigmoid()
-#         kernel_size = int(abs((math.log(self.in_planes, 2) + b) / gamma))
-#         kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
-#         # self.channel_shuffle = ChannelShuffle(groups=groups)
-#         self.con1 = nn.Conv1d(1,
-#                               1,
-#                               kernel_size=kernel_size,
-#                               padding=(kernel_size - 1) // 2,
-#                               bias=False)
-#         self.act1 = nn.Sigmoid()
-#
-#     def forward(self, x):
-#         out1 = self.avg_pool(x) + self.max_pool(x)
-#         output = out1.squeeze(-1).transpose(-1, -2)
-#         output = self.con1(output).transpose(-1, -2).unsqueeze(-1)
-#         output = self.act1(output)
-#
-#         # avg_out = self.fc(self.avg_pool(x))
-#         # max_out = self.fc(self.max_pool(x))
-#         # out = avg_out + max_out
-#         # output = torch.multiply(x, output)
-#         return output
-
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
